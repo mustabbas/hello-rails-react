1eaf763362fe1939af43ad8f21a3a275775d9877
mustabbas2000@gmail.com <mustabbas2000@gmail.com>
2022-02-22 07:14:23 +0300

    setup react-rails, showing message

---
+++ app/javascript/redux/messages/messages.js
@@ -0,0 +1,25 @@
+const GET_MESSAGE = 'GET_MESSAGE';
+
+const initialState = [];
+
+
+export const getMessage = () => (dispatch) => {
+  fetch('http://127.0.0.1:3000/messages')
+    .then((res) => res.json())
+    .then((resResponse) => dispatch({
+      type: GET_MESSAGE,
+      payload: resResponse,
+    }));
+};
+
+
+const messagesReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case GET_MESSAGE:
+      return action.payload;
+    default:
+      return state;
+  }
+};
+
+export default messagesReducer;
